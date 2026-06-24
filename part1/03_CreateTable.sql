7f64aaa98cd1c3e6dae4f712540be2ae9019eecc
Shaahzad <shehzadmemon147@gmail.com>
2026-06-24 22:32:15 +0500

    learn PostgresSql

--- part1/03_CreateTable.sql
+++ part1/03_CreateTable.sql
@@ -13,4 +13,20 @@ CREATE TABLE basics.students (
 
     -- unique means 2 students have not same email 
     email TEXT NOT NULL UNIQUE,
-)
+
+
+    age INTEGER CHECK (age >= 18),
+
+    -- TIMESTAMP store date and time
+    -- default means if u dont give any value it will take by default
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+
+--  insert some data
+
+
+INSERT INTO basics.students (name, email, age) VALUES 
+('Shahzad', 'Shahzad@gmail.com', 25),
+('Miswer', 'Miswer@gmail.com', 23),
+('Maaz', 'Maaz@gmail.com', 20)
