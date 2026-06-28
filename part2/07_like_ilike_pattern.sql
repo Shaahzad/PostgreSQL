4db569d17f11313995774baa63df7cfd673ff4e3
Shaahzad <shehzadmemon147@gmail.com>
2026-06-28 15:46:35 +0500

    learn Advance postgreSQL part 2

--- part2/07_like_ilike_pattern.sql
+++ part2/07_like_ilike_pattern.sql
@@ -1,3 +1,18 @@
 --  like case sensitive match
 --  ilike case insensitive pattern match
 --  % means any number of chars
+
+
+-- SELECT name, price
+-- FROM products
+-- WHERE name LIKE 'Gaming%';
+
+
+-- SELECT name, price
+-- FROM products
+-- WHERE name ILIKE '%mechanical%';
+
+SELECT name, category, description
+FROM products
+WHERE name ILIKE '%mechanical%'
+      OR description ILIKE '%mechanical%';
